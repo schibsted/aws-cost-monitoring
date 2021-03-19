3c5c49ee6dad5c438056e2866719726f776a1cc6
Radosław Lisowski <radoslaw.lisowski@schibsted.com>
2021-03-19 18:54:27 +0100

    fix: changed cost types to only use amortized.
    BREAKING CHANGES
    
    This change will cause your costs to be different due to the usage of
    amortized prices, and lack of support and taxes.

--- aws-budget/main.tf
+++ aws-budget/main.tf
@@ -11,6 +11,20 @@ resource "aws_budgets_budget" "monthly" {
   time_period_start = "2017-07-01_00:00"
   time_unit         = "MONTHLY"
 
+  cost_types {
+    include_credit             = false
+    include_discount           = false
+    include_other_subscription = false
+    include_recurring          = false
+    include_refund             = false
+    include_subscription       = false
+    include_support            = false
+    include_tax                = false
+    include_upfront            = false
+    use_blended                = false
+    use_amortized              = true
+  }
+
   notification {
     comparison_operator        = "GREATER_THAN"
     threshold                  = var.actual_threshold_percent
