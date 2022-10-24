e98f0e3c232b371bd38e898a10929b9237b8a12a
Radek Lisowski <radek@lisowski.me>
2022-10-24 11:00:29 +0200

    chore: upgrade datadog provider 3.0+ (#4)
    * feat: upgraded datadog provider to version 3.0.0+
    
    * fix: removed deprecated parameters to silence warnings
    
    * format: fixed tf formatting

--- datadog-budget/monitors.tf
+++ datadog-budget/monitors.tf
@@ -3,7 +3,6 @@ resource "datadog_monitor" "aws_cost_monitor" {
   name         = "Abnormal spendings on AWS service {{servicename.name}} on account ${local.alert_title_account}"
   query        = "${datadog_monitor.aws_service_anomaly.0.id} && ${datadog_monitor.aws_service_minimal_cost.0.id}"
   include_tags = true
-  locked       = false
   message      = local.datadog_monitor_message
   type         = "composite"
   tags         = var.monitor_tags
@@ -13,43 +12,37 @@ resource "datadog_monitor" "aws_service_anomaly" {
   count            = local.alerts_enabled
   evaluation_delay = 900
   include_tags     = true
-  locked           = false
   message          = "sub monitor. Check datadog_monitor.aws_cost_monitor"
 
   name                = "(sub/anomaly) Abnormal spendings on AWS service {{servicename.name}} on account ${local.alert_title_account}"
-  new_host_delay      = 300
+  new_group_delay     = 300
   notify_audit        = false
   notify_no_data      = false
   query               = "avg(last_2w):anomalies(diff(max:aws.billing.estimated_charges{${local.alert_query_from}} by {${local.query_group_by}}.rollup(max, ${var.metric_rollup_period})), '${var.anomaly_algorithm}', ${var.anomaly_algorithm_deviation}, direction='${var.anomaly_alerting_direction}', alert_window='last_1d', interval=7200, count_default_zero='true', seasonality='weekly') >= 1"
   renotify_interval   = 0
   require_full_window = false
   tags                = var.monitor_tags
-  threshold_windows = {
-    "recovery_window" = "last_1h"
-    "trigger_window"  = "last_1d"
+  monitor_threshold_windows {
+    recovery_window = "last_1h"
+    trigger_window  = "last_1d"
   }
-  thresholds = {
-    "critical"          = "1.0"
-    "critical_recovery" = "0.0"
-    "warning"           = "0.5"
+  monitor_thresholds {
+    critical          = 1.0
+    critical_recovery = 0.0
+    warning           = 0.5
   }
   timeout_h = 0
   type      = "query alert"
-
-  lifecycle {
-    ignore_changes = [silenced]
-  }
 }
 
 resource "datadog_monitor" "aws_service_minimal_cost" {
   count            = local.alerts_enabled
   evaluation_delay = 900
   include_tags     = true
-  locked           = false
   message          = "sub monitor. Check datadog_monitor.aws_cost_monitor"
 
   name                = "(sub/minimal) Abnormal spendings on AWS service {{servicename.name}} on account ${local.alert_title_account}"
-  new_host_delay      = 300
+  new_group_delay     = 300
   no_data_timeframe   = 172800
   notify_audit        = false
   notify_no_data      = true
@@ -57,15 +50,11 @@ resource "datadog_monitor" "aws_service_minimal_cost" {
   renotify_interval   = 0
   require_full_window = false
   tags                = var.monitor_tags
-  thresholds = {
-    "critical" = var.minimal_alerting_value
+  monitor_thresholds {
+    critical = var.minimal_alerting_value
   }
   timeout_h = 0
   type      = "query alert"
-
-  lifecycle {
-    ignore_changes = [silenced]
-  }
 }
 
 # Mute all sub monitors
@@ -75,7 +64,7 @@ resource "datadog_downtime" "aws_service_anomaly" {
   monitor_id = datadog_monitor.aws_service_anomaly.0.id
 
   lifecycle {
-    ignore_changes = [start, active]
+    ignore_changes = [start]
   }
 }
 
@@ -85,6 +74,6 @@ resource "datadog_downtime" "aws_service_minimal_cost" {
   monitor_id = datadog_monitor.aws_service_minimal_cost.0.id
 
   lifecycle {
-    ignore_changes = [start, active]
+    ignore_changes = [start]
   }
 }
